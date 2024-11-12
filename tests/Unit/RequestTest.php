f2cff6a914b50ffefd4468cc701daa9993ee486b
SydVic <fabrizio.bocca27@gmail.com>
2024-11-12 11:57:15 +0100

    request test

---
+++ tests/Unit/RequestTest.php
@@ -0,0 +1,22 @@
+<?php
+
+it('creates a correctly formed GET Request object', function () {
+    // ACT
+    $request = \App\Http\Request::create(
+        method: 'GET',
+        uri: '/some/path?black=white&day=night',
+        server: [
+            'CONTENT_TYPE' => 'application/json',
+            'Accept'       => 'application/json'
+        ],
+        content: ''
+    );
+
+    // ASSERT
+    expect($request->getQueryParams())
+        ->toMatchArray(['black' => 'white', 'day' => 'night',])
+        ->and($request->getPath())
+        ->toBe('some/path')
+        ->and($request->getMethod())
+        ->toBe('GET');
+});
