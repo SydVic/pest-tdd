06c716c77d05787fe4cba4008b02d24b1b47afa3
SydVic <fabrizio.bocca27@gmail.com>
2024-11-12 11:23:09 +0100

    response test

---
+++ tests/Unit/responseTest.php
@@ -0,0 +1,12 @@
+<?php
+
+test('a response object can be created', function () {
+    // ACT
+    $response = new Response('{"foo": "bar"}', 200);
+
+    // ASSERT
+    expect($response->getStatus)->toBeInt()->toBe(200)
+    ->and($response->getBody())->toMatchJson([
+        'foo' => 'bar',
+    ]);
+});
