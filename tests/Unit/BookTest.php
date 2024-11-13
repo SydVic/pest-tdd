56ac98207327ef3f7c674fc263c2b239186ba778
SydVic <fabrizio.bocca27@gmail.com>
2024-11-13 15:28:24 +0100

    Book entity

---
+++ tests/Unit/BookTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Entity\Book;
+
+test('a book can be created using named constructor', function() {
+    // ARRANGE
+    /*$author = Author::create(
+        id: 321,
+        name: 'John Doe',
+        bio: 'This is a bio'
+    );*/
+
+    // ACT
+    $book = Book::create(
+        id: 123,
+        title: 'Test book',
+        yearPublished: 1999
+        // author: $author
+    );
+
+    // ASSERT
+    expect($book)->toBeInstanceOf(Book::class)
+    ->and($book->getId())->toBe(123)
+    ->and($book->title)->toBe('Test book')
+    ->and($book->yearPublished)->toBe(1999);
+    //->and($book->author)->toBe($author);
+});
