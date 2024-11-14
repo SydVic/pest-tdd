1797a73e87ca33a29dae84457b2e4f30cfd6f1e0
SydVic <fabrizio.bocca27@gmail.com>
2024-11-14 15:49:33 +0100

    connection object

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -4,17 +4,23 @@ namespace App\Repository;
 
 use App\Entity\Author;
 use App\Entity\Book;
-use PDO;
 
 class BookRepository
 {
+    public function __construct(private Connection $connection)
+    {
+    }
+
     public function findById(int $id): ?Book
     {
         // Instantiate PDO instance
-        $dsn = 'sqlite:db/pest-tdd.sqlite';
+        /*$dsn = 'sqlite:db/pest-tdd.sqlite';
         $pdo = new PDO($dsn);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);*/
+
+        // Obtain PDO instance
+        $pdo = $this->connection->getPdo();
 
         // Prepare the statement (SQL)
         $stmt = $pdo->prepare("
